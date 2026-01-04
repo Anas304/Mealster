4f01bbdb8979ae66396a4e48a19271d43156b17c
Anas Ahmad <52326655+Anas304@users.noreply.github.com>
2026-01-04 20:47:48 +0500

    Refactor: User schema and repository setup
    -- Added UserEntity class to store and handle users.
    -- Added UserRepository and declared two functions findByEmail,findByEmailOrUsername for UserRepository enhanced functionality.
    -- Password security with BCrypt
    -- Added comment for JPA properties

--- user/src/main/kotlin/com/anas/mealster/infra/database/entities/UserEntity.kt
+++ user/src/main/kotlin/com/anas/mealster/infra/database/entities/UserEntity.kt
@@ -1,4 +1,40 @@
 package com.anas.mealster.infra.database.entities
 
-class UserEntity {
-}
+import com.anas.mealster.domain.model.UserId
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+
+@Entity
+@Table(
+    name = "users",
+    schema = "users_service",
+    indexes = [
+        Index(name = "email_idx", columnList = "email", unique = true),
+        Index(name = "username_idx", columnList = "username", unique = true)
+    ]
+)
+class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id : UserId? = null,
+    @Column(nullable = false, unique = true)
+    val email: String,
+    @Column(nullable = false, unique = true)
+    val username: String,
+    @Column(nullable = false)
+    val hashedPassword: String,
+    @Column(nullable = false)
+    val hasVerifiedEmail: Boolean = false,
+    @CreationTimestamp
+    val createdAt : Instant = Instant.now(),
+    @UpdateTimestamp
+    val updatedAt : Instant = Instant.now()
+)
--- user/src/main/kotlin/com/anas/mealster/infra/database/repository/UserRepository.kt
+++ user/src/main/kotlin/com/anas/mealster/infra/database/repository/UserRepository.kt
@@ -1,2 +1,11 @@
 package com.anas.mealster.infra.database.repository
 
+import com.anas.mealster.domain.model.UserId
+import com.anas.mealster.infra.database.entities.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<UserEntity, UserId>{
+
+    fun findByEmail(email: String) : UserEntity?
+    fun findByEmailOrUsername(email: String,username: String) : UserEntity?
+}
