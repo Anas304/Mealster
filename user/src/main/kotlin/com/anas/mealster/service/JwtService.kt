5b9e64486debeace88688c6df980b9548fcc086f
Anas Ahmad <52326655+Anas304@users.noreply.github.com>
2026-01-26 13:14:44 +0500

    Refactor: added JWT expiryMinutes in dev and prod profiles

--- user/src/main/kotlin/com/anas/mealster/service/JwtService.kt
+++ user/src/main/kotlin/com/anas/mealster/service/JwtService.kt
@@ -1,9 +1,16 @@
 package com.anas.mealster.service
 
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import kotlin.io.encoding.Base64
 
 class JwtService(
-    @param: Value("\${jwt.secret}") private val secretKey : String
+    @param: Value("\${jwt.secret}") private val secreteBase64 : String,
+    @param: Value("\${jwt.expiry.minutes}") private val expiryMinutes : Int
 ) {
+
+    private val secreteKey = Keys.hmacShaKeyFor(Base64.decode(secreteBase64))
+
+    private val accessTokenValidityMs = expiryMinutes * 60 * 1000L
 
 }
